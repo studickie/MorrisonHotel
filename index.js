2768702fa4d38f6e5af446b23ff2cdd0bd21bac1
Stu Dickie <studickiecodes@gmail.com>
2020-06-07 14:12:41 -0400

    added pug templating

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const  express = require('express')
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.listen(3000)
+app.set('view engine', 'pug')
+app.set('views', path.resolve(__dirname, './views'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(3000)
+})
