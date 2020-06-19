f3254f760e26dd993f984b108fcef53d69e4d612
Stu Dickie <studickiecodes@gmail.com>
2020-06-19 19:07:05 -0400

    removed unused packages

--- config/app.js
+++ config/app.js
@@ -12,16 +12,4 @@ app.get('/', (req, res) => {
     res.render('index');
 }); 
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.use((req, res, next) => {
-    return res.status(404).render('404');
-});
-
-app.use((err, req, res, next) => {
-    return res.status(500).render('500');
-})
-
 module.exports = app
