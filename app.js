8cdda4e788e4f88426143cec16d65c854c2bcf7e
Stu Dickie <studickiecodes@gmail.com>
2020-06-19 19:54:15 -0400

    moved startup files from config

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.resolve(__dirname, '../views'));
+app.set('views', path.resolve(__dirname, 'views'));
 
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
