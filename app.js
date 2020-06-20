325a6777fa3b5c393f136bb92c2bc48cc1c129f2
Stu Dickie <studickiecodes@gmail.com>
2020-06-20 19:09:01 -0400

    fixes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const movieRouter = require('./routes/movie.route');
 
@@ -9,6 +10,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
