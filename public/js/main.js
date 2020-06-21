0532a93722c762c08b5cf1a9b3061fd8e3fd4fb0
Stu Dickie <studickiecodes@gmail.com>
2020-06-21 13:44:52 -0400

    fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,33 @@
+function ToggleElement ($element, $state) {
+    var ctrl = this;
+    
+    //~ Expects:
+    //~     $element: HTMLElement; container element of dropdown
+    //~     $state: Boolean [optional]; initial state of dropdown
+    //~ -----------------------------------------------------------
+    ctrl._el = $element;
+    ctrl._active = false;
+
+    ctrl.active = $state || false;
+    
+    //~ Requires child with name attribute 'toggle_activator'
+    //~ -----------------------------------------------------------
+    ctrl._el.children.toggle_activator.addEventListener('click', function() { 
+        ctrl.active = !ctrl.active; 
+    });
+};
+
+ToggleElement.prototype = {
+    get active() {
+        return this._active;
+    },
+    set active(newState) {
+        this._active = newState || false;
+
+        this._el.classList.toggle('toggle--active', this._active);
+    }
+};
+
 function initToggleElements() {
     var el = document.querySelector('[data-role=dropdown]');
 
