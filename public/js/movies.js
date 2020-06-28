b15410cce1b1cd6568c7f6fc7a2153c6b36de4a0
Stu Dickie <studickiecodes@gmail.com>
2020-06-28 11:17:44 -0400

    fixed jitter while side scrolling

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,8 +1,7 @@
 var movies = {};
-movies.scrolling = false;                   //- scroll state for throtteling
-movies.scrollOffset = 0;                    //- index of visible element
-movies.scrollWidth = window.innerWidth;     //- element width; works while element is full screen width
-movies.scrollIndex = 0;   
+movies.scrolling = false;
+movies.scrollOffset = 0;
+movies.scrollWidth = window.innerWidth;
 movies.scrollList = document.querySelectorAll('[data-role=scroll_item]');
 movies.scrollChange = 0;
 //~ -------------------------
@@ -11,15 +10,11 @@ movies.scrollChange = 0;
 
 movies.addScrollEvents = function () {
     var scrollContainer = document.querySelector('.scrollContainer');
-    var eventDrag;
 
     scrollContainer.addEventListener('touchstart', function (e) {
-
+        var eventDrag;
         var scrollStart = e.touches[0].clientX;
-        console.log('Start- scrollStart: ', scrollStart);
-
         var scrollPosition = 0
-        console.log('Start- scrollPosition: ', scrollPosition);
 
         scrollContainer.addEventListener('touchmove', function eventDrag(e) {
 
@@ -27,44 +22,45 @@ movies.addScrollEvents = function () {
                 movies.scrolling = true;
 
                 movies.scrollChange = e.touches[0].clientX - scrollStart;
-                console.log('scrollChange: ', movies.scrollChange)
 
-                scrollPosition = (movies.scrollWidth * movies.scrollIndex) + (movies.scrollChange * -1);
-                console.log('scrollPosition: ', scrollPosition)
+                scrollPosition = (movies.scrollWidth * movies.scrollOffset) + (movies.scrollChange * -1);
 
-                scrollContainer.scrollTo(scrollPosition, 0);
+                scrollContainer.scroll({
+                    left: scrollPosition,
+                    behavior: 'smooth'
+                });
 
-                setTimeout(function () { movies.scrolling = false }, 17);
+                setTimeout(function () { movies.scrolling = false; }, 17);
             }
+
         });
 
         scrollContainer.addEventListener('touchend', function eventEnd() {
-            console.log('eventEnd')
-            console.log('distance needed: ',  movies.scrollWidth / 3, "distance changed: ", movies.scrollChange * -1, movies.scrollChange * -1 > movies.scrollWidth / 3)
-            
+            console.log("distance changed: ", movies.scrollChange * -1)
+
             if (movies.scrollChange * -1 > movies.scrollWidth / 3) {
-                movies.scrollIndex++;
-                var el = movies.scrollList[movies.scrollIndex];
+                movies.scrollOffset++;
+
+                var el = movies.scrollList[movies.scrollOffset];
 
                 el.scrollIntoView({
                     behavior: 'smooth',
                     inline: 'start'
                 });
             }
-            
             else {
-                var el = movies.scrollList[movies.scrollIndex];
-                
+                var el = movies.scrollList[movies.scrollOffset];
+
                 el.scrollIntoView({
                     behavior: 'smooth',
                     inline: 'start'
                 });
             }
-            
+
+            //- Reset 
+            movies.scrollChange = 0;
             scrollContainer.removeEventListener('touchmove', eventDrag);
             scrollContainer.removeEventListener('touchend', eventEnd);
-            movies.scrollChange = 0;
-            
         });
     });
 }
@@ -78,3 +74,65 @@ movies.init = function () {
 }
 
 window.addEventListener('load', movies.init);
+
+
+
+// movies.addScrollEvents = function () {
+//     var scrollContainer = document.querySelector('.scrollContainer');
+//     var eventDrag;
+
+//     scrollContainer.addEventListener('touchstart', function (e) {
+
+//         var scrollStart = e.touches[0].clientX;
+//         console.log('Start- scrollStart: ', scrollStart);
+
+//         var scrollPosition = 0
+//         console.log('Start- scrollPosition: ', scrollPosition);
+
+//         scrollContainer.addEventListener('touchmove', function eventDrag(e) {
+
+//             if (!movies.scrolling) {
+//                 movies.scrolling = true;
+
+//                 movies.scrollChange = e.touches[0].clientX - scrollStart;
+//                 console.log('scrollChange: ', movies.scrollChange)
+
+//                 scrollPosition = (movies.scrollWidth * movies.scrollIndex) + (movies.scrollChange * -1);
+//                 console.log('scrollPosition: ', scrollPosition)
+
+//                 scrollContainer.scrollBy(scrollPosition, 0);
+
+//                 setTimeout(function () { movies.scrolling = false }, 17);
+//             }
+//         });
+
+//         scrollContainer.addEventListener('touchend', function eventEnd() {
+//             console.log('eventEnd')
+//             console.log('distance needed: ',  movies.scrollWidth / 3, "distance changed: ", movies.scrollChange * -1, movies.scrollChange * -1 > movies.scrollWidth / 3)
+
+//             if (movies.scrollChange * -1 > movies.scrollWidth / 3) {
+//                 movies.scrollIndex++;
+//                 var el = movies.scrollList[movies.scrollIndex];
+
+//                 el.scrollIntoView({
+//                     behavior: 'smooth',
+//                     inline: 'start'
+//                 });
+//             }
+
+//             else {
+//                 var el = movies.scrollList[movies.scrollIndex];
+
+//                 el.scrollIntoView({
+//                     behavior: 'smooth',
+//                     inline: 'start'
+//                 });
+//             }
+
+//             scrollContainer.removeEventListener('touchmove', eventDrag);
+//             scrollContainer.removeEventListener('touchend', eventEnd);
+//             movies.scrollChange = 0;
+
+//         });
+//     });
+// }
