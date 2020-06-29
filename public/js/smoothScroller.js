951d3df7850e101428e1eed570e26ca8f34a27d8
Stu Dickie <studickiecodes@gmail.com>
2020-06-28 20:15:11 -0400

    added anchor element to highlight reel items

--- public/js/smoothScroller.js
+++ public/js/smoothScroller.js
@@ -5,7 +5,11 @@
 //~     1. $element: HTMLElement; toggle element container
 //~
 //~     Use:
-//~     > requires scroll children have attribute data-role as 
+//~     > modifies element CSS to create a smooth scroll effect
+//~         on touch events for mobile devices
+//~     > requires a list parent with attribute data-role as
+//~         'scroll_list'
+//~     > requires scroll children with attribute data-role as 
 //~         'scroll_item'
 //~ --------------------------------------------------------------
 
@@ -52,8 +56,6 @@ SmoothScroller.prototype = {
         return;
     },
     scrollToElement: function() {
-        //el.scrollIntoView({ behavior: 'smooth', inline: 'start', block: 'start' });
-        
         this._scrollList.style.transform='translate(' + (this._scrollWidth * (this._scrollIndex * -1)) + 'px)';
         
         return;
@@ -64,15 +66,21 @@ function addSmoothScrollerEvents() {
     var ctrl = this;
 
     ctrl._el.addEventListener('touchstart', function(evt) {
+        evt.preventDefault();
+
         var eventDrag;
         
         ctrl._scrollStart = evt.touches[0].clientX;
 
         ctrl._el.addEventListener('touchmove', function eventDrag(evt) {
+            evt.preventDefault();
+
             ctrl.throttledUpdateScroll(evt.touches[0].clientX);
         });
 
         ctrl._el.addEventListener('touchend', function eventEnd(evt) {
+            evt.preventDefault();
+
             ctrl.getScrollDirection();
 
             //- Reset
