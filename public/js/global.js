294cf62bd79e37cfc7da9380a791a69e3c9ef059
Stu Dickie <studickiecodes@gmail.com>
2020-09-09 20:11:01 -0400

    fixed  iframe continues playing after modal close

--- public/js/global.js
+++ public/js/global.js
@@ -23,45 +23,9 @@ mainJs.initSignoutButton = function () {
     }
 }
 
-mainJs.initModalOpenButton = function () {
-    var button = document.querySelector('a#anc_modal_open');
-
-    if (button) {
-        button.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            var key = this.getAttribute('data-src');
-            var iframe = document.querySelector('iframe#modal_video_iframe');
-
-            if (iframe) {
-                iframe.setAttribute('src', 'https://www.youtube.com/embed/' + key);
-            }
-
-            document.querySelector('#modal_video').classList.remove('modal--hidden');
-        });
-    }
-}
-
-mainJs.initModalCloseButton = function () {
-    var button = document.querySelector('button[name=btn_modal_close]');
-    
-    if (button) {
-        button.addEventListener('click', function () {
-            var modalContainer = document.querySelector('#modal_video');
-
-            if (modalContainer) {
-                //document.querySelector('main').removeChild(modalContainer);
-                modalContainer.classList.add('modal--hidden');
-            }
-        });
-    }
-}
-
 mainJs.init = function () {
     mainJs.initNavMenuToggle();
     mainJs.initSignoutButton();
-    mainJs.initModalOpenButton();
-    mainJs.initModalCloseButton();
 }
 
 window.addEventListener('load', mainJs.init);
